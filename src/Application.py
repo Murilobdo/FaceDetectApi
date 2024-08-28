5b219bd2561c507ae639d9ff5ad9f035069aa947
= <murilobdo@hotmail.com>
2024-08-27 22:12:24 -0300

    feautre(Glasses): inserindo reconhecimento de oculos de leitura ou de sol na foto da pessoa

--- src/Application.py
+++ src/Application.py
@@ -1,30 +1,37 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 from services.FaceDetector import FaceDetector
 from services.Response import Response
 
 app = Flask(__name__)
+CORS(app)
+
+def NotFound():
+    return jsonify({ 'data': False,'statusCode': 404 })
+
+def BadRequest():
+    return jsonify({ 'data': False,'statusCode': 400 })
+
+def Ok(result):
+    return jsonify({ 'data': result,'statusCode': 200 })
+
 
 @app.route('/status', methods=['GET'])
 def status():
-    return jsonify({'status': 'API is running!'})
+    return Ok('API is running!')
 
 @app.route('/validate', methods=['POST'])
 def validate():
     try:
+        if(len(request.files) == 0):
+            return NotFound()
         image = request.files['image']
         faceDetector = FaceDetector(image)
         result = faceDetector.detect()
-        response = Response(200, result)
-        return jsonify({ 
-                        'data': result,
-                        'statusCode': 200
-                    })
+        return Ok(result)
     except Exception as e:
-        return jsonify({ 
-                        'data': False,
-                        'statusCode': 400
-                    })
+        return BadRequest(str(e))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7400)
--- src/services/FaceDetector.py
+++ src/services/FaceDetector.py
@@ -4,13 +4,16 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from PIL import Image
 from ultralytics import YOLO
+import matplotlib.pyplot as plt
+import matplotlib.image as imgplt
 import os
-
-
+import cv2
+import random
+from glasses_detector import GlassesClassifier, GlassesDetector
 
 class FaceDetector:
     def __init__(self, image):
-        self.result = False
+        self.result = ''
         self.image = np.array(Image.open(image))
         
     def detect(self):
@@ -18,12 +21,10 @@ class FaceDetector:
         FaceDetector = mp.tasks.vision.FaceDetector
         FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
         VisionRunningMode = mp.tasks.vision.RunningMode
-
         
         current_dir = os.path.dirname(os.path.abspath(__file__))
         model_path = os.path.normpath(os.path.join(current_dir, '../models/blaze_face_short_range.tflite'))
 
-        
         options = FaceDetectorOptions(
             base_options=BaseOptions(model_asset_path=model_path),
             running_mode=VisionRunningMode.IMAGE)
@@ -33,33 +34,18 @@ class FaceDetector:
             detection_result = detector.detect(mp_image)
             
             qtd = len(detection_result.detections)
-            
-            if(qtd == 1):
-                self.result = True
+   
+            _ = GlassesClassifier(kind='anyglasses')
+            predictGlasses = _.predict(self.image)
+                
+            if(qtd == 1 and predictGlasses == 'absent'):
+                self.result = 'Foto Validada com Sucesso'
+            elif(qtd == 1 and predictGlasses == 'present'):
+                self.result = 'Foto com Óculos'
             else:
-                self.result = False
+                self.result = 'Nenhuma pessoa foi reconhecida na foto'
         
         return self.result
-    
-    def yoloDetect(self):
-        model = YOLO('yolov8n.pt')
-        results = model(self.image)
-        detections = []
-        for result in results:
-            for box in result.boxes:
-                cls = int(box.cls[0])  # Índice da classe detectada
-                conf = box.conf[0].item()  # Confiança da detecção
-                bbox = box.xyxy[0].tolist()  # Coordenadas do bounding box [xmin, ymin, xmax, ymax]
-                
-                if cls == 15:  # Exemplo: o índice 15 pode ser "sunglasses" no seu modelo
-                    detections.append({
-                        "class": cls,
-                        "confidence": conf,
-                        "bbox": bbox
-                    })
-                    
-        if(len(detections) == 0):
-            self.result = f'Foto validada com sucesso'
-        else:
-            self.result = 'Foto obstruida'
+
+   
             
